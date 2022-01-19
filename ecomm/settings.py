9705f1f216a1cc3946276f722757e07904a86925
krunaldrj <59255802+krunaldrj@users.noreply.github.com>
2022-01-19 23:51:15 +0530

    new

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,4 +123,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Configure Django App for Heroku.
+import django_heroku
 django_heroku.settings(locals())
